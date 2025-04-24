13f05fe80e2cec5b96d4e6ebc6580a98116e102f
bex <30572280+JoshPaulie@users.noreply.github.com>
2025-04-24 18:21:15 -0500

    fix: don't overwrite existing (original) file (resolves #32)

--- src/nano_encoder/commands/untag.py
+++ src/nano_encoder/commands/untag.py
@@ -12,12 +12,24 @@ from .base_command import BaseCommand
 def handle_untag_command(args: UntagArgs) -> None:
     try:
         UntagDirectory(args).execute()
-    except (FileNotFoundError, NotADirectoryError, ValueError) as e:
+    except (FileNotFoundError, NotADirectoryError, ValueError, UntaggedVideoOverwriteError) as e:
         logger.error(str(e))
         raise
     except KeyboardInterrupt:
         message = "User cancelled untag operation."
+        console.print(f"\n{message}\n")
         logger.info(message)
+
+
+class UntaggedVideoOverwriteError(Exception):
+    """
+    An exception for when the "would-be-untagged" would overwrite and existing file.
+
+    Effectively, this means it won't overwrite the original if it's still present.
+    """
+
+    def __init__(self, video_name: str) -> None:
+        super().__init__(f"{video_name} already exists. Consider running purge command.")
 
 
 class UntagDirectory(BaseCommand):
@@ -46,4 +58,7 @@ class UntagDirectory(BaseCommand):
     @staticmethod
     def _untagged_name(video: Path) -> Path:
         """Generate the untagged version of the filename."""
-        return video.with_name(video.name.replace(".optimized", ""))
+        untagged_video = video.with_name(video.name.replace(".optimized", ""))
+        if untagged_video.exists():
+            raise UntaggedVideoOverwriteError(untagged_video.name)
+        return untagged_video
